4742fa0d0f465cae859a24fc88b9eef668f2ce1e
fauzirentah <fauzi.rentah@gmail.com>
2022-02-19 22:45:35 +0800

    initial DAO

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x51c6ee867Fcd070f25412A9b218d48fD4988a905";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
